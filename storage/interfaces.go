4d040c9af5e3cc98b880e2d47243fc7f25bbcd6b
Nick K <u@colonpipe.biz>
2016-04-03 13:21:32 +0300

    Created border-package transactions for the storage

--- storage/interfaces.go
+++ storage/interfaces.go
@@ -1,9 +1,30 @@
 package storage
 
-// ReadWriter is able to retrieve values and write them back.
-type ReadWriter interface {
-	Get(key string) ([]byte, error)
-	Set(key string, value []byte) error
-	Unset(key string) error
+// Reader is able to retrieve values from the storage.
+type Reader interface {
+	Get(key string) (string, error)
 	NumEqualTo(key string) (uint64, error)
+}
+
+// Writer is able to write values to the storage.
+type Writer interface {
+	Set(key string, value string) error
+	Unset(key string) error
+}
+
+// ReadWriter is able to read and modify values.
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+// DB is an instance of a database, or transaction over it.
+type DB interface {
+	ReadWriter
+	// Tx creates a transaction over the database or current transaction.
+	Tx() DB
+	// Commit commits the transaction tree, returning database's root.
+	Commit() (DB, error)
+	// Rollback cancels the transaction, returning parent tx (or database's root).
+	Rollback() (DB, error)
 }
