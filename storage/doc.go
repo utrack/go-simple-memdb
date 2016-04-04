b12ffb8fb886be3cdf0f81bd856a7c5ce135fc5a
Nick K <u@colonpipe.biz>
2016-04-04 12:55:34 +0300

    Fixed docs' newlines

--- storage/doc.go
+++ storage/doc.go
@@ -8,16 +8,21 @@ Use New() to instantiate the storage.
 Basic usage
 
 Storage supports two basic operations - Set and Unset.
+
 Set adds or modifies the variable by its key. Existing variable is overwritten.
+
 Unset removes the variable.
+
 Storage supports count-index by variables' values - use NumEqualTo to count variables
 with given values.
 
 Transactions
 
 Storage supports transactions with unlimited nesting.
+
 When the transaction is committed it traverses its parents all the way down to the root storage,
 effectively committing the whole Tx tree.
+
 Rollback rolls back only one transaction, returning its parent.
 
 See DB interface for the API and usage examples.
